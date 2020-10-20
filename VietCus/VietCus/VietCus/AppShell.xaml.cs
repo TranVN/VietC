c29532f58e16aac961ac8d726c38c6c586f2e923
TranVN <coppership1@gmail.com>
2020-10-20 15:06:43 +0700

    New Project

---
+++ VietCus/VietCus/VietCus/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using VietCus.ViewModels;
+using VietCus.Views;
+using Xamarin.Forms;
+
+namespace VietCus
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ VietCus/VietCus/VietCus/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using VietCus.ViewModels;
+
+namespace VietCus.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
